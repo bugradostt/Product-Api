54b8400637111ef5bb76e023343128e34f2aa3f8
bugradostt <bugradostt@gmail.com>
2024-06-05 23:10:04 +0300

    Creating layers

---
+++ Product.DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.DataAccess.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<ProductEntity> Products { get; set; }
+
+
+    }
+}
